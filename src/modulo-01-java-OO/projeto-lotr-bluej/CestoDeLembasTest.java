ec605cdc56043fe4160abda2305644b60cbe96bf
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-04-06 14:26:11 -0300

    no message

--- src/modulo-01-java-OO/projeto-lotr-bluej/CestoDeLembasTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/CestoDeLembasTest.java
@@ -5,13 +5,21 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-class CestoDeLembasTest
+public class CestoDeLembasTest
 {
     @Test
     public void criarCestoCom2Lembas(){
         CestoDeLembas cesto = new CestoDeLembas(2);
         boolean esperado = false;
         boolean obtido = cesto.podeDividirEmPares();
-        assertEquals(obtido,esperado);
+        assertEquals(esperado,obtido);
+    }
+    
+     @Test
+    public void criarCestoCom4Lembas(){
+        CestoDeLembas cesto = new CestoDeLembas(4);
+        boolean esperado = false;
+        boolean obtido = cesto.podeDividirEmPares();
+        assertEquals(esperado,obtido);
     }
 }
