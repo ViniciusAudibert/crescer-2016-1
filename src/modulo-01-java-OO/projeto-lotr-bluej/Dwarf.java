90425387cfaa88daf8277124554bebb9aef84c73
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-04-06 16:09:56 -0300

    feat (Dwarf) Implementação dos getters e setters

--- src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
@@ -10,4 +10,12 @@ public class Dwarf {
     public void perdeVida(){
         this.vida -= 10;
     }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
 }
