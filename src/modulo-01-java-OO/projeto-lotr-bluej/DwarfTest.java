aad4de0c852c4fad19b117c67412eda7c12aa812
Vinicius Audibert <vini.audibert@gmail.com>
2016-04-07 19:04:46 -0300

    feat(DwarfTest): Novo método testaConstrutor2 e modificação no testaContrutor

--- src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
@@ -9,6 +9,39 @@ public class DwarfTest {
         Dwarf dwarf = new Dwarf("Jumbeto");
         assertEquals(110, dwarf.getVida());
         assertEquals("Jumbeto", dwarf.getNome());
+        assertTrue(dwarf.getStatus() == Status.VIVO);
+        assertTrue(dwarf.getDataNascimento().getDia() == 1);
+        assertTrue(dwarf.getDataNascimento().getMes() == 1);
+        assertTrue(dwarf.getDataNascimento().getAno() == 1);
+    }
+    
+    @Test
+     public void testaConstrutor2(){
+        DataTerceiraEra dataNascimento = new DataTerceiraEra(12,7,2015);
+        Dwarf dwarf = new Dwarf("Jumbeto", dataNascimento);
+        assertEquals(110, dwarf.getVida());
+        assertEquals("Jumbeto", dwarf.getNome());
+        assertTrue(dwarf.getStatus() == Status.VIVO);
+        assertTrue(dwarf.getDataNascimento().getDia() == 12);
+        assertTrue(dwarf.getDataNascimento().getMes() == 7);
+        assertTrue(dwarf.getDataNascimento().getAno() == 2015);
+    }
+    
+    @Test
+    public void testaAdicionarItem(){
+        Dwarf dwarf = new Dwarf("Jumbeto");
+        Item item = new Item(1,"Sword");
+        dwarf.adicionarItem(item);
+        assertTrue(dwarf.getInventario().getItens().get(0) == item);
+    }
+    
+    @Test
+    public void testaPerderItem(){
+        Dwarf dwarf = new Dwarf("Jumbeto");
+        Item item = new Item(1,"Sword");
+        dwarf.adicionarItem(item);
+        dwarf.perderItem(item);
+        assertTrue(dwarf.getInventario().getItens().isEmpty());
     }
     
     @Test
