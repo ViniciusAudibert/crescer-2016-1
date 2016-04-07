34b7ec608253f0caf6e2f6b76e38fd9e5de4f556
Vinicius Audibert <vini.audibert@gmail.com>
2016-04-07 09:27:03 -0300

    Refactory (DataTerceiraEraTest) Troca de dados

--- src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTest.java
@@ -5,20 +5,20 @@ import org.junit.Test;
 
 public class DataTerceiraEraTest {
     @Test
-    public void testaEhBissexto366(){
-        DataTerceiraEra d1 = new DataTerceiraEra(366,0,0);
+    public void testaEhBissextoFevereiro29(){
+        DataTerceiraEra d1 = new DataTerceiraEra(29,2,0);
         assertEquals(d1.ehBissexto(), true);
     }
     
     @Test
-    public void testaEhBissexto365(){
-        DataTerceiraEra d2 = new DataTerceiraEra(365,0,0);
+    public void testaEhBissextoFevereiro29(){
+        DataTerceiraEra d2 = new DataTerceiraEra(28,2,0);
         assertEquals(d2.ehBissexto(), false);
     }
     
     @Test
-    public void testaEhBissexto400(){
-        DataTerceiraEra d3 = new DataTerceiraEra(400,0,0);
+    public void testaEhBissextoJaneiro31(){
+        DataTerceiraEra d3 = new DataTerceiraEra(31,1,0);
         assertEquals(d3.ehBissexto(), false);
     }
     
