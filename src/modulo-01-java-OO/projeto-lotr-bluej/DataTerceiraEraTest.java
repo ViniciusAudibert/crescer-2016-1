99c80e66579bba80fb796a1ce9f58d49f8d7a475
Vinicius Audibert <vini.audibert@gmail.com>
2016-04-07 00:01:39 -0300

    Refactory (DataTerceiraEraTest) Dividido em vários métodos

--- src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTest.java
@@ -5,12 +5,21 @@ import org.junit.Test;
 
 public class DataTerceiraEraTest {
     @Test
-    public void testaEhBissexto(){
-        DataTerceiraEra d1 = new DataTerceiraEra(0,0,366);
-        DataTerceiraEra d2 = new DataTerceiraEra(0,0,365);
-        DataTerceiraEra d3 = new DataTerceiraEra(0,0,400);
+    public void testaEhBissexto366(){
+        DataTerceiraEra d1 = new DataTerceiraEra(366,0,0);
         assertEquals(d1.ehBissexto(), true);
+    }
+    
+    @Test
+    public void testaEhBissexto365(){
+        DataTerceiraEra d2 = new DataTerceiraEra(365,0,0);
         assertEquals(d2.ehBissexto(), false);
+    }
+    
+    @Test
+    public void testaEhBissexto400(){
+        DataTerceiraEra d3 = new DataTerceiraEra(400,0,0);
         assertEquals(d3.ehBissexto(), false);
     }
+    
 }
