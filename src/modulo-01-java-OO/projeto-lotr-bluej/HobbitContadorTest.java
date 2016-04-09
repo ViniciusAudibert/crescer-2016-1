3114efd1d32963fa6607ab5c62debd4f3f3a0c8e
ViniciusAudibert <vini.audibert@gmail.com>
2016-04-09 00:23:47 -0300

    feat(HobbitContadorTest): Teste com seis pares no método calculaDiferenca.

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContadorTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContadorTest.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class HobbitContadorTest {
     @Test
-    public void calcularDiferenca(){
+    public void calcularDiferencaComTresPares(){
         ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
         arrayDePares.add(new ArrayList<>(Arrays.asList(15, 18)));
         arrayDePares.add(new ArrayList<>(Arrays.asList(4, 5)));
@@ -15,5 +15,20 @@ public class HobbitContadorTest {
         HobbitContador contador = new HobbitContador();
         int diferenca = contador.calcularDiferenca(arrayDePares);
         assertTrue(diferenca == 840);
+    }
+    
+    @Test
+    public void calcularDiferencaComSeisPares(){
+        ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
+        arrayDePares.add(new ArrayList<>(Arrays.asList(13, 20)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(40, 40)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(14, 20)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(4, 16)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(1, 28)));           
+        arrayDePares.add(new ArrayList<>(Arrays.asList(60, 7)));
+        
+        HobbitContador contador = new HobbitContador();
+        int diferenca = contador.calcularDiferenca(arrayDePares);
+        assertTrue(diferenca == 1748);
     }
 }
