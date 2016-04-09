05edba3b6a940f51d3ec4f8e7d2252804eaf1605
ViniciusAudibert <vini.audibert@gmail.com>
2016-04-09 00:02:28 -0300

    feat(HobbitContador): Criação das classes HobbitContador e HobbitContadorTest. feat(calculaDiferenca): Novo método que retorna a soma da diferença entre o produto e o mínimo múltiplo comum de cada par no array.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class HobbitContador {
+    public int calcularDiferenca(ArrayList<ArrayList<Integer>> arrayDePares){
+        int somaDaDiferenca = 0;
+        ArrayList<Integer> produtoDoArray = new ArrayList<>();
+        ArrayList<Integer> mmcDoArray = new ArrayList<>();
+        for(int i=0; i<arrayDePares.size(); i++){
+            produtoDoArray.add(arrayDePares.get(i).get(0) * arrayDePares.get(i).get(1));
+        }
+        for(int i=0; i<arrayDePares.size(); i++){
+            for(int j=1; true; j++){
+                if((j % arrayDePares.get(i).get(0) == 0) && (j % arrayDePares.get(i).get(1) == 0)){
+                    mmcDoArray.add(j);
+                    break;
+                }
+            }
+        }
+        for(int i=0; i<arrayDePares.size(); i++){
+            somaDaDiferenca += produtoDoArray.get(i) - mmcDoArray.get(i);
+        }
+        return somaDaDiferenca;
+    }
+}
