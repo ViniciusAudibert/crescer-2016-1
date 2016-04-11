142c24b98ab30434c326e8c08cbfa571641c70a7
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-04-11 17:17:51 -0300

    test(ElfoVerdeTest): Testa o método adicionaItem.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoVerdeTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoVerdeTest.java
@@ -3,7 +3,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ElfosVerdesTest {
+public class ElfoVerdeTest {
     @Test
     public void atiraUmaFlecha(){
         Elfo elfo = new ElfoVerde("Orelha");
@@ -35,5 +35,32 @@ public class ElfosVerdesTest {
         assertEquals(117, elfo.getFlechas());
         assertEquals(6, elfo.getExperiencia());
         assertEquals(dwarf.getVida(),80);
+    }
+    
+    @Test
+    public void adicionarUmItemPermitido(){
+        ElfoVerde elfo = new ElfoVerde("Jumbeto");
+        Item item = new Item(1,"Espada de aço valiriano");
+        elfo.adicionarItem(item);
+        assertTrue(elfo.getInventario().getItens().get(0) == item);
+    }
+    
+    @Test
+    public void adicionarUmItemNaoPermitido(){
+        ElfoVerde elfo = new ElfoVerde("Ranger");
+        Item item = new Item(1,"Sword");
+        elfo.adicionarItem(item);
+        assertTrue(elfo.getInventario().getItens().isEmpty());
+    }
+    
+    @Test
+    public void adicionarDoisItensPermitidos(){
+        ElfoVerde elfo = new ElfoVerde("Ranger");
+        Item item = new Item(1,"Espada de aço valiriano");
+        Item item2 = new Item(1,"Arco e Flecha de Vidro");
+        elfo.adicionarItem(item);
+        elfo.adicionarItem(item2);
+        assertTrue(elfo.getInventario().getItens().get(0) == item);
+        assertTrue(elfo.getInventario().getItens().get(1) == item2);
     }
 }
