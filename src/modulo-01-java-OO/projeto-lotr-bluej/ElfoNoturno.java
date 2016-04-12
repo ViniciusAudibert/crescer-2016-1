fd26a77c91bc9d558ada014565f01fe8492d7eaa
ViniciusAudibert <vini.audibert@gmail.com>
2016-04-12 09:57:34 -0300

    fix(ElfoNoturno): Atributo privado vidaReal que irá conter a vida precisa do elfo.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
@@ -1,26 +1,24 @@
 public class ElfoNoturno extends Elfo {
-    private double vida;
+    private double vidaReal;
     public ElfoNoturno(String nome){
         super(nome);
-        vida = atributos.getVida();
+        this.vidaReal = atributos.getVida();
     }
     public ElfoNoturno(String nome, int flechas){
         super(nome, flechas);
-        vida = atributos.getVida();
+        this.vidaReal = atributos.getVida();
     }
     
     public void atirarFlecha(Dwarf dwarf){ // Elfos Noturnos nao morrem pois nao existe um parametro que cheque se sua vida está zerada, pois entao ele poderia continuar até suas flechas acabarem.
         if(atributos.getStatus() == Status.VIVO){
             super.atirarFlecha(dwarf);
             atributos.experiencia += 2;
-            this.vida -= this.vida * 0.05;
-            if(this.vida <= 0){
+            this.vidaReal -= this.vidaReal * 0.05;
+            atributos.vida = (int) this.vidaReal;
+            if(atributos.vida < 1){
                 atributos.setStatus(Status.MORTO);
+                atributos.vida = 0;
             }
         }
-    }
-    
-    public double getVida(){
-        return this.vida;
-    }
+    }   
 }
