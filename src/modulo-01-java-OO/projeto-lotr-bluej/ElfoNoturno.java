3b332a5ade73d13c4011ffabcb5a0d4c0de290c6
ViniciusAudibert <vini.audibert@gmail.com>
2016-04-12 00:21:52 -0300

    fix(ElfoNoturno): Atributo vida vira double para mais precisão.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
@@ -1,19 +1,26 @@
 public class ElfoNoturno extends Elfo {
+    private double vida;
     public ElfoNoturno(String nome){
         super(nome);
+        vida = atributos.getVida();
     }
     public ElfoNoturno(String nome, int flechas){
         super(nome, flechas);
+        vida = atributos.getVida();
     }
     
     public void atirarFlecha(Dwarf dwarf){ // Elfos Noturnos nao morrem pois nao existe um parametro que cheque se sua vida está zerada, pois entao ele poderia continuar até suas flechas acabarem.
         if(atributos.getStatus() == Status.VIVO){
             super.atirarFlecha(dwarf);
             atributos.experiencia += 2;
-            atributos.vida -= atributos.getVida() * 0.05;
-            if(atributos.getVida() <= 0){
+            this.vida -= this.vida * 0.05;
+            if(this.vida <= 0){
                 atributos.setStatus(Status.MORTO);
             }
         }
+    }
+    
+    public double getVida(){
+        return this.vida;
     }
 }
