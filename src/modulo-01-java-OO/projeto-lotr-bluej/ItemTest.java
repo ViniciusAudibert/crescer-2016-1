795b932a54df2a9be3a34df63b8ac751179d10e9
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-04-12 15:52:33 -0300

    test(ItemTest): Testa o método equals.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ItemTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ItemTest.java
@@ -6,9 +6,23 @@ import org.junit.Test;
 public class ItemTest
 {
     @Test
-    public void contrutor(){
-        Item item = new Item(2, "Sword");
-        assertTrue(item.getQuantidade() == 2);
-        assertTrue(item.getDescricao() == "Sword");
+    public void coparaItensIguais(){
+        Item item1 = new Item(2, "Sword");
+        Item item2 = new Item(2, "Sword");
+        assertTrue(item1.equals(item2));
+    }
+    
+    @Test
+    public void coparaItensDiferentes(){
+        Item item1 = new Item(2, "Sword");
+        Item item2 = new Item(3, "Sword");
+        assertFalse(item1.equals(item2));
+    }
+    
+    @Test
+    public void coparaUmItemNull(){
+        Item item1 = new Item(2, "Sword");
+        Item item2 = null;
+        assertFalse(item1.equals(item2));
     }
 }
