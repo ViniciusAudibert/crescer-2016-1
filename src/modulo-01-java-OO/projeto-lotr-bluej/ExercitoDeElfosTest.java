7665487c33e09be9a4e05bbfb395e255acbc9341
ViniciusAudibert <vini.audibert@gmail.com>
2016-04-14 01:08:59 -0300

    test(ExercitoDeElfosTest): Testa todos os métodos da classe ExercitoDeElfos.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfosTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfosTest.java
@@ -38,26 +38,39 @@ public class ExercitoDeElfosTest {
     public void agruparPorStatus(){
         ExercitoDeElfos exercito = new ExercitoDeElfos();
         Elfo e1 = new ElfoVerde("Ranger");
-        Elfo e2 = new ElfoVerde("Nopturne",120);
+        Elfo e2 = new ElfoVerde("Nopturne");
         Elfo e3 = new ElfoNoturno("Legolas",120);
         Elfo e4 = new ElfoNoturno("Night One",120);
-        Elfo e5 = new ElfoNoturno("Star Arrower");
         exercito.alistaElfo(e1);
         exercito.alistaElfo(e2);
         exercito.alistaElfo(e3);
         exercito.alistaElfo(e4);
-        exercito.alistaElfo(e5);
         for(int i=0; i<90; i++){
             e3.atirarFlecha(new Dwarf("Morto"));
             e4.atirarFlecha(new Dwarf("Morto"));
         }
         exercito.agruparPorStatus();
-        assertTrue(exercito.buscaPeloStatus(Status.MORTO).contains("Legolas"));
-        assertTrue(exercito.buscaPeloStatus(Status.MORTO).contains("Night One"));
+        assertTrue(exercito.getExercitoDeElfosStatus().get(Status.MORTO).contains(e4));
+        assertTrue(exercito.getExercitoDeElfosStatus().get(Status.VIVO).contains(e2));
     }
     
     @Test
     public void buscaPeloStatus(){
-    
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        Elfo e1 = new ElfoVerde("Ranger");
+        Elfo e2 = new ElfoVerde("Nopturne");
+        Elfo e3 = new ElfoNoturno("Legolas",120);
+        Elfo e4 = new ElfoNoturno("Night One",120);
+        exercito.alistaElfo(e1);
+        exercito.alistaElfo(e2);
+        exercito.alistaElfo(e3);
+        exercito.alistaElfo(e4);
+        for(int i=0; i<90; i++){
+            e3.atirarFlecha(new Dwarf("Morto"));
+            e4.atirarFlecha(new Dwarf("Morto"));
+        }
+        exercito.agruparPorStatus();
+        assertTrue(exercito.buscaPeloStatus(Status.MORTO).contains(e4));
+        assertTrue(exercito.buscaPeloStatus(Status.VIVO).contains(e1));
     }
 }
