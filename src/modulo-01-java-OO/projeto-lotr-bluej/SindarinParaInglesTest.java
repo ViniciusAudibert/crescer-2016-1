96d449a8be40440118bd302d960374df6f494758
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-04-14 16:42:58 -0300

    test(SindarinParaInglesTest, SindarinParaPortuguesTest): Testar se o método traduzir retorna a palavra correspondente e se não existe retornar null.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/SindarinParaInglesTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SindarinParaInglesTest {
+    @Test
+    public void traduzirNauParaIngles(){
+        TradutorSindarin tradutorIngles = new SindarinParaIngles();
+        assertEquals("fire",tradutorIngles.traduzir("naur"));
+    }
+    
+    @Test
+    public void traduzirNullParaIngles(){
+        TradutorSindarin tradutorIngles = new SindarinParaIngles();
+        assertNull(tradutorIngles.traduzir("jabulaine"));
+    }
+}
---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/SindarinParaPortuguesTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SindarinParaPortuguesTest {
+    @Test
+    public void traduzirNauParaPortugues(){
+        TradutorSindarin tradutorPortugues = new SindarinParaPortugues();
+        assertEquals("fogo",tradutorPortugues.traduzir("naur"));
+    }
+    
+    @Test
+    public void traduzirNullParaPortugues(){
+        TradutorSindarin tradutorPortugues = new SindarinParaPortugues();
+        assertNull(tradutorPortugues.traduzir("jabulaine"));
+    }
+}
