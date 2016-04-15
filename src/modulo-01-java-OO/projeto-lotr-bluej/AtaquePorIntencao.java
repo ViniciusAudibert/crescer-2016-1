2ad81db4e3620772522e21f06d42ae6e8f620995
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-04-15 14:49:49 -0300

    feat(AtaqueNoturnoPorIntencao): Retorna array com a ordem em que os elfos atacarão.

--- src/modulo-01-java-OO/projeto-lotr-bluej/AtaquePorIntencao.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/AtaquePorIntencao.java
@@ -1,19 +1,22 @@
 import java.util.*;
 public class AtaquePorIntencao implements Estrategia {
+    private ArrayList<Elfo> ordemDeAtaque = new ArrayList<Elfo>();
+    int contagemDosAtaques = 0;
+    
     @Override
     public void atacar(ExercitoDeElfos exercito, ArrayList<Dwarf> dwarfList){
         exercito.agruparPorStatus();
         ArrayList<Elfo> elfosVivos = exercito.getExercitoDeElfosStatus().get(Status.VIVO);
         int intencaoDeAtaque = elfosVivos.size() * dwarfList.size();
         intencaoDeAtaque *= 0.3;
-        int contagemDosAtaques = 0;
         for(Elfo elfo : elfosVivos){
             if(elfo instanceof ElfoVerde){
                 this.tiraVidaDosDwarfs(dwarfList,elfo);
+                ordemDeAtaque.add(elfo);
             }
             else if((contagemDosAtaques < intencaoDeAtaque)&& elfo instanceof ElfoNoturno){
                 this.tiraVidaDosDwarfs(dwarfList,elfo);
-                contagemDosAtaques++;
+                ordemDeAtaque.add(elfo);
             }
         }
     }
@@ -22,6 +25,12 @@ public class AtaquePorIntencao implements Estrategia {
     public void tiraVidaDosDwarfs(ArrayList<Dwarf> dwarfList, Elfo elfo){
         for(Dwarf dwarf : dwarfList){
             elfo.atirarFlecha(dwarf);
+            contagemDosAtaques++;
         }
+    }
+    
+    @Override
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque(){     
+        return ordemDeAtaque;
     }
 }
