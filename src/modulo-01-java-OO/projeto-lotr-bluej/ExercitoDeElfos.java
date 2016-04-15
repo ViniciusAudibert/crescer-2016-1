3f75e1f682751c65afcdce3d02cddad2ae49c6c0
ViniciusAudibert <vini.audibert@gmail.com>
2016-04-15 00:49:40 -0300

    feat(ExercitoDeElfos): Ataca uma horda de dwarfs com estrategia em que elfos noturnos atarão 30% da intenção de vezes.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfos.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfos.java
@@ -40,6 +40,29 @@ public class ExercitoDeElfos {
         return listaDeElfosPorStatus;
     }
     
+    private void tiraVidaDosDwarfs(ArrayList<Dwarf> dwarfList, Elfo elfo){
+        for(Dwarf dwarf : dwarfList){
+            elfo.atirarFlecha(dwarf);
+        }
+    }
+    
+    public void atacarDwarfsEstrategiaPorIntencoes(ArrayList<Dwarf> dwarfList){
+        this.agruparPorStatus();
+        ArrayList<Elfo> elfosVivos = exercitoDeElfosStatus.get(Status.VIVO);
+        int intencaoDeAtaque = elfosVivos.size() * dwarfList.size();
+        intencaoDeAtaque *= 0.3;
+        int contagemDosAtaques = 0;
+        for(Elfo elfo : elfosVivos){
+            if(elfo instanceof ElfoVerde){
+                this.tiraVidaDosDwarfs(dwarfList,elfo);
+            }
+            else if((contagemDosAtaques < intencaoDeAtaque)&& elfo instanceof ElfoNoturno){
+                this.tiraVidaDosDwarfs(dwarfList,elfo);
+                contagemDosAtaques++;
+            }
+        }
+    }
+    
     public HashMap<String,Elfo> getExercitoDeElfos(){
         return this.exercitoDeElfos;
     }
