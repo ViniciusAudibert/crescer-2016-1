a10cd0a51e230e03002441072b07c06aa0206e79
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-04-15 14:50:43 -0300

    feat(Estrategia): Implementação de um metodo que irá retornar um array com a ordem que os elfos atacarão.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Estrategia.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Estrategia.java
@@ -2,4 +2,5 @@ import java.util.*;
 public interface Estrategia {
     public void atacar(ExercitoDeElfos exercito, ArrayList<Dwarf> dwarfList);
     public void tiraVidaDosDwarfs(ArrayList<Dwarf> dwarfList, Elfo elfo);
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque();
 }
