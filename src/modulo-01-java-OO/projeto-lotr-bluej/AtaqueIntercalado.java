af8e4e144ff955fc6650584974cac7f2ad0e2eef
ViniciusAudibert <vini.audibert@gmail.com>
2016-04-15 03:45:59 -0300

    feat(AtaqueIntercalado): Estrategia de ataque onde fica intercalando entre ataque do ElfoVerde e ElfoNoturno.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/AtaqueIntercalado.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class AtaqueIntercalado implements Estrategia {
+    public void atacar(ExercitoDeElfos exercito, ArrayList<Dwarf> dwarfList){
+        exercito.agruparPorStatus();
+        ArrayList<Elfo> elfosVivos = exercito.getExercitoDeElfosStatus().get(Status.VIVO);
+        if(this.igualdadeDeElfos(elfosVivos)){
+            exercito.ordenaArrayDeElfosVivos(elfosVivos);
+            for(int i=0; i<elfosVivos.size()/2; i++){
+                this.tiraVidaDosDwarfs(dwarfList,elfosVivos.get(i));
+                this.tiraVidaDosDwarfs(dwarfList,elfosVivos.get(i + elfosVivos.size()/2));
+            }
+        }
+    }
+    
+    @Override
+    public void tiraVidaDosDwarfs(ArrayList<Dwarf> dwarfList, Elfo elfo){
+        for(Dwarf dwarf : dwarfList){
+            elfo.atirarFlecha(dwarf);
+        }
+    }
+    
+    private boolean igualdadeDeElfos(ArrayList<Elfo> elfosVivos){
+        int contElfoVerde = 0;
+        int contElfoNoturno = 0;
+        for(Elfo elfo : elfosVivos){
+            if(elfo instanceof ElfoVerde){contElfoVerde++;}
+            else if(elfo instanceof ElfoNoturno){contElfoNoturno++;}
+        }
+        return contElfoVerde - contElfoNoturno == 0;
+    }
+}
