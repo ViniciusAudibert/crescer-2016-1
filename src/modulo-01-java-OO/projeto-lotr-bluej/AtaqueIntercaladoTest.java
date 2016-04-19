3f1c63ac372c56561751201fcd9be269a1d75132
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-04-19 13:43:33 -0300

    test(AtaqueIntercaladoTest): Testa se está ordenando o arrayList.

--- src/modulo-01-java-OO/projeto-lotr-bluej/AtaqueIntercaladoTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/AtaqueIntercaladoTest.java
@@ -29,6 +29,30 @@ public class AtaqueIntercaladoTest {
         
     }
     
+    private void alistaUmElfoVerdePrimeiro(ExercitoDeElfos exercito){
+        exercito.alistaElfo(new ElfoNoturno("Fredyl"));
+        exercito.alistaElfo(new ElfoNoturno("Gorroko"));
+        exercito.alistaElfo(new ElfoNoturno("Nephil"));
+        exercito.alistaElfo(new ElfoVerde("Ranger")); // HashCode deste elfo é o primeiro no arrayList dos vivos
+        exercito.alistaElfo(new ElfoNoturno("Yagmoto"));
+        exercito.alistaElfo(new ElfoVerde("Mitamo"));
+        exercito.alistaElfo(new ElfoVerde("Mitsume"));
+        exercito.alistaElfo(new ElfoVerde("Karazuko"));
+        
+    }
+    
+    private void alistaUmElfoNoturnoPrimeiro(ExercitoDeElfos exercito){
+        exercito.alistaElfo(new ElfoVerde("Fredyl"));
+        exercito.alistaElfo(new ElfoNoturno("Gorroko"));
+        exercito.alistaElfo(new ElfoVerde("Nephil"));
+         exercito.alistaElfo(new ElfoNoturno("Ranger")); // HashCode deste elfo é o primeiro no arrayList dos vivos
+        exercito.alistaElfo(new ElfoVerde("Yagmoto"));
+        exercito.alistaElfo(new ElfoVerde("Mitamo"));
+        exercito.alistaElfo(new ElfoNoturno("Mitsume"));
+        exercito.alistaElfo(new ElfoNoturno("Karazuko"));
+        
+    }
+    
     @Test
     public void exercitoAtacaDwarfsEstrategiaPorIntercalacaoSemIgualdade(){
         ExercitoDeElfos exercitoElfos = new ExercitoDeElfos();
@@ -54,15 +78,34 @@ public class AtaqueIntercaladoTest {
     }
     
     @Test
-    public void exercitoAtacaDwarfsEstrategiPorIntercalacaoGetOrdem(){
+    public void atacaDwarfsEstrategiaPorIntercalacaoGetOrdemVerdePrimeiro(){
         ExercitoDeElfos exercitoElfos = new ExercitoDeElfos();
         ArrayList<Dwarf> exercitoDwarfs = exercitoDeDwarfs();
         Estrategia estrategia = new AtaqueIntercalado();
-        alistaElfosMisturadosComIgualdade(exercitoElfos);
+        alistaUmElfoVerdePrimeiro(exercitoElfos);
         estrategia.atacar(exercitoElfos,exercitoDwarfs); 
-        assertEquals(estrategia.getOrdemDoUltimoAtaque().get(0).getNome(), "Ranger");
-        assertEquals(estrategia.getOrdemDoUltimoAtaque().get(1).getNome(), "Fredyl");
-        assertEquals(estrategia.getOrdemDoUltimoAtaque().get(2).getNome(), "Nephil");
-        assertEquals(estrategia.getOrdemDoUltimoAtaque().get(3).getNome(), "Gorroko");
+        int metadeDoTamanhoDoArray = exercitoElfos.getExercitoDeElfosStatus().size()/2;
+        for(int i=0; i<metadeDoTamanhoDoArray; i++){
+            Elfo elfoVerde = exercitoElfos.getExercitoDeElfosStatus().get(Status.VIVO).get(i);
+            Elfo elfoNoturno = exercitoElfos.getExercitoDeElfosStatus().get(Status.VIVO).get(i + metadeDoTamanhoDoArray);
+            assertTrue(elfoVerde instanceof ElfoVerde);
+            assertTrue(elfoNoturno instanceof ElfoNoturno);
+        }
+    }
+    
+    @Test
+    public void atacaDwarfsEstrategiaPorIntercalacaoGetOrdemNoturnoPrimeiro(){
+        ExercitoDeElfos exercitoElfos = new ExercitoDeElfos();
+        ArrayList<Dwarf> exercitoDwarfs = exercitoDeDwarfs();
+        Estrategia estrategia = new AtaqueIntercalado();
+        alistaUmElfoNoturnoPrimeiro(exercitoElfos);
+        estrategia.atacar(exercitoElfos,exercitoDwarfs); 
+        int metadeDoTamanhoDoArray = exercitoElfos.getExercitoDeElfosStatus().size()/2;
+        for(int i=0; i<metadeDoTamanhoDoArray; i++){       
+            Elfo elfoNoturno = exercitoElfos.getExercitoDeElfosStatus().get(Status.VIVO).get(i);  
+            Elfo elfoVerde = exercitoElfos.getExercitoDeElfosStatus().get(Status.VIVO).get(i + metadeDoTamanhoDoArray);
+            assertTrue(elfoNoturno instanceof ElfoNoturno);
+            assertTrue(elfoVerde instanceof ElfoVerde);
+        }
     }
 }
