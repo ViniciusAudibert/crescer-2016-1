f46d48354698c55a48145c2460012dc40cc61d0b
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-05-03 12:47:51 -0300

    test(ExercitoDeElfosTest): implementação do tratamento NaoPodeAlistarException e mais cenarios de teste

--- src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfosTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfosTest.java
@@ -7,35 +7,29 @@ import java.util.*;
 
 public class ExercitoDeElfosTest {
     @Test
-    public void alistaElfos(){
+    public void alistaElfos() throws NaoPodeAlistarException{
         ExercitoDeElfos exercito = new ExercitoDeElfos();
-        Elfo e1 = new Elfo("Ranger");
-        Elfo e2 = new ElfoVerde("Nopturne");
-        Elfo e3 = new ElfoNoturno("Legolas");
+        Elfo e1 = new ElfoVerde("Nopturne");
+        Elfo e2 = new ElfoNoturno("Legolas");
         exercito.alistaElfo(e1);
         exercito.alistaElfo(e2);
-        exercito.alistaElfo(e3);
-        assertNull(exercito.getExercitoDeElfos().get("Ranger"));
-        assertEquals(exercito.getExercitoDeElfos().get("Nopturne"),e2);
-        assertEquals(exercito.getExercitoDeElfos().get("Legolas"),e3);
+        assertEquals(exercito.getExercitoDeElfos().get("Nopturne"),e1);
+        assertEquals(exercito.getExercitoDeElfos().get("Legolas"),e2);
     }
     
     @Test
-    public void buscaElfo(){
+    public void buscaElfo() throws NaoPodeAlistarException{
         ExercitoDeElfos exercito = new ExercitoDeElfos();
-        Elfo e1 = new Elfo("Ranger");
-        Elfo e2 = new ElfoVerde("Nopturne");
-        Elfo e3 = new ElfoNoturno("Legolas");
+        Elfo e1 = new ElfoVerde("Nopturne");
+        Elfo e2 = new ElfoNoturno("Legolas");
         exercito.alistaElfo(e1);
         exercito.alistaElfo(e2);
-        exercito.alistaElfo(e3);
-        assertNull(exercito.buscaElfo("Ranger"));
-        assertEquals(exercito.buscaElfo("Nopturne"),e2);
-        assertEquals(exercito.buscaElfo("Legolas"),e3);
+        assertEquals(exercito.buscaElfo("Nopturne"),e1);
+        assertEquals(exercito.buscaElfo("Legolas"),e2);
     }
     
     @Test
-    public void agruparPorStatus(){
+    public void agruparPorStatus() throws NaoPodeAlistarException{
         ExercitoDeElfos exercito = new ExercitoDeElfos();
         Elfo e1 = new ElfoVerde("Ranger");
         Elfo e2 = new ElfoVerde("Nopturne");
@@ -55,7 +49,7 @@ public class ExercitoDeElfosTest {
     }
     
     @Test
-    public void buscaPeloStatus(){
+    public void buscaPeloStatus() throws NaoPodeAlistarException{
         ExercitoDeElfos exercito = new ExercitoDeElfos();
         Elfo e1 = new ElfoVerde("Ranger");
         Elfo e2 = new ElfoVerde("Nopturne");
@@ -75,7 +69,7 @@ public class ExercitoDeElfosTest {
     }
     
     @Test
-    public void agrupaPorStatusDuasVezes(){
+    public void agrupaPorStatusDuasVezes() throws NaoPodeAlistarException{
         ExercitoDeElfos exercito = new ExercitoDeElfos();
         Elfo e1 = new ElfoNoturno("Ranger",120);
         Elfo e2 = new ElfoNoturno("Nopturne",120);
@@ -99,5 +93,79 @@ public class ExercitoDeElfosTest {
         assertFalse(exercito.getExercitoDeElfosStatus().get(Status.VIVO).contains(e1));
         assertFalse(exercito.getExercitoDeElfosStatus().get(Status.VIVO).contains(e2));
         assertFalse(exercito.getExercitoDeElfosStatus().get(Status.MORTO).contains(e3));
+    }
+      
+    @Test
+    public void buscarElfoPorNomeInexistente() throws NaoPodeAlistarException {
+        Elfo elfo = new ElfoNoturno("Night Legolas");
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.alistaElfo(elfo);
+        assertNull(exercito.buscaElfo("Birinight Legolas"));
+    }
+
+    @Test
+    public void buscarElfoPorNomeInexistenteExercitoVazio() {
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        assertNull(exercito.buscaElfo("Birinight Legolas"));
+    }
+
+    @Test(expected=NaoPodeAlistarException.class)
+    public void buscarElfoNomeValidoTipoInvalido() throws NaoPodeAlistarException {
+        Elfo elfo = new Elfo("Night Legolas");
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.alistaElfo(elfo);
+        assertNull(exercito.buscaElfo("Night Legolas"));
+    }
+
+    @Test
+    public void buscaElfosVivos() throws NaoPodeAlistarException {
+        ElfoVerde elfoVivo1 = new ElfoVerde("Green 1");
+        ElfoNoturno elfoVivo2 = new ElfoNoturno("Aa");
+        ElfoVerde elfoVivo3 = new ElfoVerde("BB");
+        Elfo elfoMorto1 = criarElfoNoturnoMorto("Dead Elf 1");
+        Elfo elfoMorto2 = criarElfoNoturnoMorto("Dead Elf 2");
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.alistaElfo(elfoMorto1);
+        exercito.alistaElfo(elfoMorto2);
+        exercito.alistaElfo(elfoVivo1);
+        exercito.alistaElfo(elfoVivo2);
+        exercito.alistaElfo(elfoVivo3);
+        exercito.agruparPorStatus();
+        ArrayList<Elfo> vivos = exercito.busca(Status.VIVO);
+        assertTrue(vivos.contains(elfoVivo1));
+        assertTrue(vivos.contains(elfoVivo2));
+        assertTrue(vivos.contains(elfoVivo3));
+    }
+
+    @Test
+    public void buscaElfosMortos() throws NaoPodeAlistarException {
+        ExercitoDeElfos exercito = criarExercitoDeMortosEVivos();
+        exercito.agruparPorStatus();
+        ArrayList<Elfo> mortos = exercito.busca(Status.MORTO);
+        assertTrue(mortos.contains(exercito.buscaElfo("Dead Elf 1")));
+        assertTrue(mortos.contains(exercito.buscaElfo("Dead Elf 2")));
+    }
+
+    private ExercitoDeElfos criarExercitoDeMortosEVivos() throws NaoPodeAlistarException {
+        ElfoVerde elfoVivo1 = new ElfoVerde("Green 1");
+        ElfoNoturno elfoVivo2 = new ElfoNoturno("Aa");
+        ElfoVerde elfoVivo3 = new ElfoVerde("BB");
+        Elfo elfoMorto1 = criarElfoNoturnoMorto("Dead Elf 1");
+        Elfo elfoMorto2 = criarElfoNoturnoMorto("Dead Elf 2");
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.alistaElfo(elfoMorto1);
+        exercito.alistaElfo(elfoMorto2);
+        exercito.alistaElfo(elfoVivo1);
+        exercito.alistaElfo(elfoVivo2);
+        exercito.alistaElfo(elfoVivo3);
+        return exercito;
+    }
+    
+    private Elfo criarElfoNoturnoMorto(String nome) {
+        Elfo elfo = new ElfoNoturno(nome,100);
+        for (int i = 0; i < 90; i++) {
+            elfo.atirarFlecha(new Dwarf("Gimli"));
+        }
+        return elfo;
     }
 }
