211fbf6c17683e11349a949bfda97459eb4cee0f
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-05-04 13:05:29 -0300

    test(lista-js-specs): testa o script dos exercicios da aula 2.

--- src/modulo-04-javascript/Exercicios aula 02/lista-js-specs.js
+++ src/modulo-04-javascript/Exercicios aula 02/lista-js-specs.js
@@ -14,3 +14,39 @@ describe('Ex 2. obterCavaleiroComMaisGolpes!', function() {
     expect(obterCavaleiroComMaisGolpes().nome).toEqual('Shaka');
   })
 })
+
+describe('Ex 3. obterMesesComMaisAniversarios!', function() {
+  it('Retorna um array com os meses que possuem mais aniversiariantes', function() {
+    expect(obterMesesComMaisAniversarios()).toEqual(['março','maio','novembro']);
+  })
+})
+
+describe('Ex 4. obterAlturaMedia!', function() {
+  it('Retorna a altura media em metros dos cavaleiros', function() {
+    expect(obterAlturaMedia()).toEqual(1.86);
+  })
+})
+
+describe('Ex 5. obterAlturaMediana!', function() {
+  it('Retorna a altura mediana em metros dos cavaleiros', function() {
+    expect(obterAlturaMediana()).toEqual(1.85);
+  })
+})
+
+describe('Ex 6. obterPesoMedio!', function() {
+  it('Retorna o peso medio em kg apenas de cavaleiros com o atributo pesoLb', function() {
+    expect(obterPesoMedio()).toEqual(84.27);
+  })
+})
+
+describe('Ex 7. obterIMC!', function() {
+  it('Retorna um array com todos os IMCs dos cavaleiros com o pesoLb registrado', function() {
+    expect(obterIMC()).toEqual([22.64, 29.48, 24.62, 24.22, 24.84, 20.53, 24.54, 24.31, 23.99, 22.45, 21.5]);
+  })
+})
+
+describe('Ex 8. obterSobrepeso!', function() {
+  it('Retorna os cavaleiros com o indice IMC acima do peso', function() {
+    expect(obterSobrepeso()[0].nome).toEqual('Aldebaran');
+  })
+})
