60bfb9dc534efb409029f3f3f8d36ce2ae144880
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-05-06 17:40:49 -0300

    feat(MegaMen): criação da classe abstrata Robo.

---
+++ src/modulo-05-.NET/Exercício 1 - MegaMan/Exercicio1/Exercicio1/Robo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    public abstract class Robo
+    {
+        public abstract String Nome { get; }
+        public int Vida { get; private set; }
+        protected int Ataque;
+        protected int Defesa;
+
+        public Robo()
+        {
+            Vida = 100;
+            Ataque = 5;
+            Defesa = 0;
+        }
+        public void Atacar(Robo robo)
+        {
+            robo.Vida -= Ataque - Defesa;
+        }
+    }
+}
