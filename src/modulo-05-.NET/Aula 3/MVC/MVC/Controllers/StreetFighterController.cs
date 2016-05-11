b25c150f3b24252b46a86354e9eb698654fa1113
ViniciusAudibert <vini.audibert@gmail.com>
2016-05-11 01:02:12 -0300

    feat(MVC): concluir street fighter em .NET.

---
+++ src/modulo-05-.NET/Aula 3/MVC/MVC/Controllers/StreetFighterController.cs
@@ -0,0 +1,52 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class StreetFighterController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FichaTecnica()
+        {
+            return View(SobreMimModels());
+        }
+
+        public ActionResult Sobre()
+        {
+            return View(SobreMimModels());
+        }
+
+        SobreMimModel SobreMimModels()
+        {
+                return new SobreMimModel()
+                {
+                    PrimeiraAparicao = "Crescer",
+                    Nascimento = new DateTime(1996,11,12),
+                    Altura = 1.82,
+                    Peso = 80,
+                    Medidas = "B198, C120, Q172",
+                    TipoSanguineo = 'O',
+                    HabilidadesEspeciais = "Programar",
+                    Gosta = "Series,Ficção e Animes",
+                    Desgosta = "Spiders",
+                    EstiloDeLuta = "Mental",
+                    Origem = "Brasil",
+                    FalaDeVitoria = "Ver você em ação é uma piada!",
+                    SSF2Nickname = "A selvagem criança da natureza",
+                    SFA3Nickname = "A animal pessoa amazônica",
+                    SF4Nickname = "Guerreiro da selva",
+                    SFA3Stage = "Ramificação do Rio Madeira - pantano, Brasil",
+                    SF2Stage = "Bacia do rio Amazonas (Brasil).",
+                    GolpesEspeciais = "Hello World"
+                };
+        }
+    }
+}
---
+++ src/modulo-05-.NET/Aula 3/MVC/MVC/Models/SobreMimModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class SobreMimModel
+    {
+        public string PrimeiraAparicao { get; set; }
+        public DateTime Nascimento { get; set; }
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+        public string Medidas { get; set; }
+        public char TipoSanguineo { get; set; }
+        public string HabilidadesEspeciais { get; set; }
+        public string Gosta { get; set; }
+        public string Desgosta { get; set; }
+        public string EstiloDeLuta { get; set; }
+        public string Origem { get; set; }
+        public string FalaDeVitoria { get; set; }
+        public string SSF2Nickname { get; set; }
+        public string SFA3Nickname { get; set; }
+        public string SF4Nickname { get; set; }
+        public string SFA3Stage { get; set; }
+        public string SF2Stage { get; set; }
+        public string GolpesEspeciais { get; set; }
+    }
+}
