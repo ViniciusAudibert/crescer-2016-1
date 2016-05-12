e75543b8d8a6eaf208dbcf968a05da03f0316ce9
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-05-12 18:58:35 -0300

    feat(LojaNinja): trabalho parcial mvc da loja ninja.

---
+++ src/modulo-05-.NET/Aula 5/LojaNinja/LojaNinja.MVC/Controllers/PedidoController.cs
@@ -0,0 +1,58 @@
+﻿using LojaNinja.Dominio;
+using LojaNinja.MVC.Models;
+using LojaNinja.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaNinja.MVC.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private RepositorioVendas repositorio = new RepositorioVendas();
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Salvar(PedidoModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var pedido = new Pedido(model.DataEntrega,model.NomeProduto,model.Valor,model.TipoPagamento,model.NomeCliente,model.Cidade,model.Estado);
+                    repositorio.IncluirPedido(pedido);
+                } 
+                catch(ArgumentException ex)
+                {
+                    ModelState.AddModelError("", ex.Message);
+                    return View("Cadastro", model);
+                }
+
+                return View("Detalhes", model);
+            }
+            else
+            {
+                return View("Cadastro", model);
+            }
+        }
+
+        public ActionResult Detalhes(int id)
+        {
+            var pedido = repositorio.ObterPedidoPorId(id);
+
+            return View(pedido);
+        }
+
+        public ActionResult Listagem()
+        {
+            var pedidos = repositorio.ObterPedidos();
+
+            return View(pedidos);
+        }
+    }
+}
