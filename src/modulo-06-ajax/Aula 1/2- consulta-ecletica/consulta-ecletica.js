9c4d8cdde378e4553a23bb352741562b6d2d2dcc
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-05-20 15:08:01 -0300

    refactory(consulta-ecletica): reestruturação da função procurarArtista.

--- src/modulo-06-ajax/Aula 1/2- consulta-ecletica/consulta-ecletica.js
+++ src/modulo-06-ajax/Aula 1/2- consulta-ecletica/consulta-ecletica.js
@@ -3,20 +3,20 @@ $(function(){
   $formArtista.submit(function(e){
     var formData = new FormData($formArtista[0]);
     var id = procurarArtista(formData.get('artista'));
-    if(typeof id === 'undefined'){
-      id = '';
-    }
     carregarTodasImagens(id);
     return e.preventDefault();
   })
 })
 
 function procurarArtista(artista) {
-  var urlArista = 'https://api.spotify.com/v1/search?q=' + artista.replaceAll(" ", "%20") + '&type=artist';
+  var urlArtista = 'https://api.spotify.com/v1/search?q=' + artista.replaceAll(" ", "%20") + '&type=artist';
   var id;
-  $.ajax({url:urlArista}).then(
-    function onSuccess(e){
+  $.ajax({url:urlArtista}).then(
+    function(e){
       id = e.artists.items[0].id;
+    },
+    function onError(e){
+      id = '';
     }
   )
   return id;
@@ -34,5 +34,8 @@ function carregarTodasImagens(id){
           $imgAlbunsList.append($('<li>').append($('<img>').attr('src',element.url)));
         })
       })
+    },
+    function onError(e){
+      alert('Artista não encontrado')
     })
   }
