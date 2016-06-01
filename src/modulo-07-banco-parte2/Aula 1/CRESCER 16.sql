a8213d1b0256c94800aef00a8df698e163886a4f
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-06-01 15:26:57 -0300

    fix(SQL Oracle - Aula 1): correção dos exercicios.

--- src/modulo-07-banco-parte2/Aula 1/CRESCER 16.sql
+++ src/modulo-07-banco-parte2/Aula 1/CRESCER 16.sql
@@ -23,13 +23,13 @@ INSERT INTO PRODUTO(IDPRODUTO,NOME,DATACADASTRO,PRECOCUSTO,PRECOVENDA,SITUACAO) 
 --5) Identifique e liste os produtos que não tiveram nenhum pedido, considere os relacionamentos no modelo de dados, pois não há relacionamento direto entre Produto e Pedido (será preciso relacionar PedidoItem).
 --Obs.: o produto criado anteriormente deverá ser listado (apenas este)
 
-SELECT PR.NOME FROM PRODUTO PR LEFT JOIN PEDIDOITEM PI ON PI.IDPRODUTO = null;
+SELECT PR.NOME FROM PRODUTO PR WHERE NOT EXISTS(SELECT 1 FROM PEDIDOITEM PI WHERE PI.IDPRODUTO = PR.IDPRODUTO);
 
 --6) Liste todos os pedidos de um determinado cliente, considere que sempre que for executar esta consulta será informado o IDCliente como parâmetro. Deverão ser listados: Data do Pedido, Produto, Quantide, Valor Unitário, e valor total.
 --Exemplo: SELECT Nome FROM Cliente WHERE IDCliente = :pIDCliente
 --Neste exemplo será solicitado que informe o parâmetro para execução da consulta.
 
-SELECT PR.DATACADASTRO,PR.NOME,PI.QUANTIDADE,PI.PRECOUNITARIO,PI.PRECOUNITARIO * PI.QUANTIDADE AS PRECOTOTAL FROM PEDIDO P JOIN CLIENTE C ON C.IDCLIENTE = P.IDCLIENTE JOIN PEDIDOITEM PI ON PI.IDPEDIDO = P.IDPEDIDO JOIN PRODUTO PR ON PR.IDPRODUTO = PI.IDPRODUTO WHERE C.IDCLIENTE = :pIDCLIENTE; 
+SELECT P.DATAPEDIDO,PR.NOME,PI.QUANTIDADE,PI.PRECOUNITARIO,PI.PRECOUNITARIO * PI.QUANTIDADE AS PRECOTOTAL FROM PEDIDO P JOIN CLIENTE C ON C.IDCLIENTE = P.IDCLIENTE JOIN PEDIDOITEM PI ON PI.IDPEDIDO = P.IDPEDIDO JOIN PRODUTO PR ON PR.IDPRODUTO = PI.IDPRODUTO WHERE C.IDCLIENTE = :pIDCLIENTE; 
 
 --7) Faça uma consulta que receba um parâmetro sendo o IDProduto e liste a quantidade de itens na tabela PedidoItem com este IDProduto foram vendidos no último ano (desde janeiro/2016).
 
@@ -39,10 +39,4 @@ SELECT PI.QUANTIDADE FROM PEDIDOITEM PI JOIN PRODUTO PR ON PR.IDPRODUTO = PI.IDP
 --8) Utilizando de funções de agrupamento (aggregation function), faça uma consulta que liste agrupando por ano e mês a quantidade de pedidos comprados, a quantidade de produtos distintos comprados, o valor total dos pedidos, o menor valor de um pedido, o maior valor de um pedido e valor médio de um pedido.
 --(Dica: a função TO_CHAR permite converter Dates em String considerando formatos específicos).
 
-SELECT TO_CHAR(P.DATAPEDIDO, 'MON-YYYY') AS DATAPEDIDO, SUM(P.VALORPEDIDO) AS VALORTOTAL ,COUNT(1) AS QUANTIDADETOTOALPRODUTOS, MIN(P.VALORPEDIDO) AS MENORVALOR, MAX(P.VALORPEDIDO) AS MAIORVALOR, TO_CHAR(AVG(P.VALORPEDIDO), 'fm9999999.90') AS PRECOMEDIO FROM PEDIDO P JOIN PEDIDOITEM PI ON P.IDPEDIDO = PI.IDPEDIDO GROUP BY PI.IDPRODUTO, TO_CHAR(P.DATAPEDIDO, 'MON-YYYY');
-
-SELECT * FROM PEDIDO;
-
-SELECT * FROM PRODUTO;
-
-SELECT PI.IDPRODUTO, COUNT(1) FROM PEDIDOITEM PI JOIN PRODUTO PR ON PR.IDPRODUTO = PI.IDPRODUTO GROUP BY PI.IDPRODUTO;
+SELECT TO_CHAR(P.DATAPEDIDO, 'MON-YYYY') AS DATAPEDIDO, COUNT(DISTINCT PI.IDProduto ) AS PRODUTOS_DISTINTOS ,SUM(P.VALORPEDIDO) AS VALOR_PEDIDO ,COUNT(1) AS QUANTIDADETOTOALPRODUTOS, MIN(P.VALORPEDIDO) AS MENORVALOR, MAX(P.VALORPEDIDO) AS MAIORVALOR, TO_CHAR(AVG(P.VALORPEDIDO), 'fm9999999.90') AS PRECOMEDIO FROM PEDIDO P JOIN PEDIDOITEM PI ON P.IDPEDIDO = PI.IDPEDIDO GROUP BY TO_CHAR(P.DATAPEDIDO, 'MON-YYYY');
