b0edaab27878756bed3574c12cdce3f8dc8e76f2
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-06-02 17:36:23 -0300

    feat(SQL Oracle - Dia 3): Exercicos incompletos do dia 3.

---
+++ src/modulo-07-banco-parte2/Aula 1/CRESCER 16 Exercicio 3.sql
@@ -0,0 +1,68 @@
+--1) Consultando cliente
+--? Faça um bloco PL/SQL que receba UM parâmetro (em tempo de execução apenas): IDCliente, e então pesquise nome do cliente e cidade (caso tenha), e outras informações referente a pedidos:
+--o Data da primeira compra (pedido) realizado;
+--o Data da última compra realizada.
+--o Valor total dos pedidos.
+DECLARE
+  vNome               VARCHAR2(30);
+  vCidade             VARCHAR2(30);
+  vDataUltimaCompra   DATE;
+  vDataPrimeiraCompra DATE;
+  vValorPedido        NUMERIC(12,2);
+BEGIN
+  SELECT CL.NOME,
+    C.NOME,
+    MIN(P.DATAPEDIDO),
+    MAX(P.DATAPEDIDO),
+    SUM(P.VALORPEDIDO)
+  INTO vNome,
+    vCidade,
+    vDataPrimeiraCompra,
+    vDataUltimaCompra,
+    vValorPedido
+  FROM PEDIDO P
+  JOIN CLIENTE CL
+  ON CL.IDCLIENTE = P.IDCLIENTE
+  LEFT JOIN CIDADE C
+  ON C.IDCIDADE      = CL.IDCIDADE
+  WHERE CL.IDCLIENTE = :pIDCLIENTE
+  GROUP BY CL.NOME,
+    C.NOME;
+  DBMS_OUTPUT.put_line('Cliente: ' || vNome || ' Cidade: ' || vCidade || ' Primeira compra: ' || vDataPrimeiraCompra || ' Ultima compra: ' || vDataUltimaCompra || ' Valor total: ' || vValorPedido);
+END;
+--2) Procedimento para inserir dados
+--? Faça um bloco PL/SQL que receba DOIS parâmetros (em tempo de execução apenas): nome e uf, verifique se já existe um registro em Cidade para a combinação, caso não exista faça um INSERT na tabela de Cidade.
+--o Utilize uma SEQUENCE para gerar o próximo ID válido;
+--o Ignore o case sensitive na validação.
+--o Se já existir a cidade+uf deve imprimir uma mensagem informando.
+DECLARE
+  vCidade VARCHAR2(20);
+  vUF     VARCHAR2(2);
+  vCount  INTEGER;
+BEGIN
+  vCidade := '&_Cidade';
+  vUF     := '&_UF';
+  SELECT COUNT(1)
+  INTO vCount
+  FROM CIDADE
+  WHERE LOWER(NOME) = LOWER(vCIDADE)
+  AND UPPER(UF)     = UPPER(vUF);
+  if(vCount = 0) THEN
+  INSERT
+  INTO CIDADE
+    (
+      IDCIDADE,
+      NOME,
+      UF
+    )
+    VALUES
+    (
+      SQCIDADE.nextval,
+      vCIDADE,
+      vUF
+    );
+    COMMIT;
+    ELSE
+      DBMS_OUTPUT.PUT_LINE('Essa cidade já existe');
+    END IF;
+END;
