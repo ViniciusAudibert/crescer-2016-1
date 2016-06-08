659067e56c905118c29a34b5b153a156250817b8
Vinicius Audibert <vini.audibert@gmail.com>
2016-06-08 01:57:41 -0300

    feat(MeuStringUtil): adicionar classe MeuStringUtil.

---
+++ src/modulo-08-java/Aula 1/ExerciciosTema/src/main/java/br/com/crescer/exerciciostema/MeuStringUtil.java
@@ -0,0 +1,62 @@
+package br.com.crescer.exerciciostema;
+
+import java.lang.reflect.Method;
+
+
+public class MeuStringUtil {
+
+    public static boolean isEmpty(String string) {
+        return string == null ? true : string.trim().length() > 0 ? false : true;
+    }
+
+    public static int numVogais(String string) {
+        if (isNull(string)) {
+            return 0;
+        }
+
+        int count = 0;
+
+        for (char letra : string.toCharArray()) {
+            if (Character.toString(letra).matches("(?i)[^aeiou]+")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static String inverter(String string) {
+        if(isNull(string)){
+            return null;
+        }
+        
+        StringBuilder inverter = new StringBuilder();
+        
+        for (int i = string.length() - 1; i >= 0; i++) {
+            inverter.append(string.charAt(i));
+        }
+
+        return inverter.toString();
+    }
+    
+    public static boolean isPalindromo(String string){
+        if(isNull(string)){
+            return false;
+        }
+        
+        String invertida = inverter(string);
+        
+        return string.equals(invertida);
+    }
+    
+    private static boolean isNull(String string){
+        return string == null;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Metodos disponiveis na classe MeuStringUtil\n");
+
+        for (Method name : MeuStringUtil.class.getMethods()) {
+            System.out.println(name.getName());
+        }
+    }
+}
