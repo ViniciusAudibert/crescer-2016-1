30c97f9d0c666ac8a81a976edd2e61a73cabf734
Vinicius Audibert <vinicius.audibert@cwinet.local>
2016-06-17 12:33:20 -0300

    feat(Cidade): adicionar entidade cidade do banco.

---
+++ src/modulo-08-java/Aula 8/crescer-entity/src/main/java/br/com/crescer/entity/Cidade.java
@@ -0,0 +1,103 @@
+package br.com.crescer.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author vinicius.audibert
+ */
+@Entity
+@Table(name = "CIDADE")
+@NamedQueries({
+    @NamedQuery(name = "Cidade.findAll", query = "SELECT c FROM Cidade c"),
+    @NamedQuery(name = "Cidade.findByIdcidade", query = "SELECT c FROM Cidade c WHERE c.idcidade = :idcidade"),
+    @NamedQuery(name = "Cidade.findByNome", query = "SELECT c FROM Cidade c WHERE c.nome = :nome"),
+    @NamedQuery(name = "Cidade.findByUf", query = "SELECT c FROM Cidade c WHERE c.uf = :uf")})
+public class Cidade implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @SequenceGenerator(name = "SEQ_CIDADE", sequenceName = "SEQ_CIDADE", allocationSize = 1)
+    @Column(name = "IDCIDADE")
+    private BigDecimal idcidade;
+    @Basic(optional = false)
+    @Column(name = "NOME")
+    private String nome;
+    @Basic(optional = false)
+    @Column(name = "UF")
+    private String uf;
+
+    public Cidade() {
+    }
+
+    public Cidade(BigDecimal idcidade) {
+        this.idcidade = idcidade;
+    }
+
+    public Cidade(BigDecimal idcidade, String nome, String uf) {
+        this.idcidade = idcidade;
+        this.nome = nome;
+        this.uf = uf;
+    }
+
+    public BigDecimal getIdcidade() {
+        return idcidade;
+    }
+
+    public void setIdcidade(BigDecimal idcidade) {
+        this.idcidade = idcidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idcidade != null ? idcidade.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cidade)) {
+            return false;
+        }
+        Cidade other = (Cidade) object;
+        if ((this.idcidade == null && other.idcidade != null) || (this.idcidade != null && !this.idcidade.equals(other.idcidade))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.crescer.entity.Cidade[ idcidade=" + idcidade + " ]";
+    }
+    
+}
