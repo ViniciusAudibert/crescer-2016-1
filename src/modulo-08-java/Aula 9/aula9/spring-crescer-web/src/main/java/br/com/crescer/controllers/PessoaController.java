3d716ede0467bf8573694a232700934cf8e7f4d9
ViniciusAudibert <vini.audibert@gmail.com>
2016-06-19 18:53:08 -0300

    feat(cadastro-pessoa): adicionar cadastro pessoa.

--- src/modulo-08-java/Aula 9/aula9/spring-crescer-web/src/main/java/br/com/crescer/controllers/PessoaController.java
+++ src/modulo-08-java/Aula 9/aula9/spring-crescer-web/src/main/java/br/com/crescer/controllers/PessoaController.java
@@ -2,30 +2,56 @@ package br.com.crescer.controllers;
 
 import br.com.crescer.entity.Pessoa;
 import br.com.crescer.services.PessoaService;
+import java.math.BigInteger;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
  * @author vinicius.audibert
  */
 @Controller
+@RequestMapping(value = "/pessoa")
 public class PessoaController {
 
     @Autowired
     PessoaService service;
 
-    @RequestMapping(value = "/cadastro-pessoa")
-    String toCadastro(Model model) {
-        return "cadastro-pessoa";
-    }
-    @RequestMapping(value = "/consulta-pessoa")
-    String toConsulta(Model model) {
-        String f = null;
-        Iterable<Pessoa> pessoass = service.toList();
+    @RequestMapping(value = "" ,method = RequestMethod.GET)
+    public String toConsulta(Model model) {
         model.addAttribute("pessoas", service.toList());
         return "consulta-pessoa";
+    }
+    
+    @RequestMapping(value = "/cadastro-pessoa", method = RequestMethod.GET)
+    public String toCadastro(Model model) {
+        model.addAttribute("pessoa", new Pessoa(Long.valueOf(0),"",BigInteger.valueOf(0),new Date()));
+        return "cadastro-pessoa";
+    }
+    
+    @RequestMapping(value = "/{id}/editar-pessoa", method = RequestMethod.GET)
+    public String toEditar(@PathVariable(value = "id") Long id, Model model) {
+        Pessoa p = service.findById(Long.valueOf(id));
+        model.addAttribute("pessoa", p);
+        return "cadastro-pessoa";
+    }
+    
+    @RequestMapping(value = "/salvar", method = RequestMethod.POST)
+    public ModelAndView salvar(@ModelAttribute Pessoa p, @RequestParam("id") Long id, Model model) {
+        p.setDataNascimento(new Date());
+        if(id == null)
+            p.setIdpessoa(Long.valueOf(0));
+        else
+            p.setIdpessoa(id);
+        service.save(p);
+        return new ModelAndView("redirect:/pessoa");
     }
 }
--- src/modulo-08-java/Aula 9/aula9/spring-crescer-web/src/main/java/br/com/crescer/services/PessoaService.java
+++ src/modulo-08-java/Aula 9/aula9/spring-crescer-web/src/main/java/br/com/crescer/services/PessoaService.java
@@ -17,17 +17,25 @@ import org.springframework.stereotype.Service;
 public class PessoaService {
 
     @Autowired
-    PessoaRepositorio pessoaaRep;
-    
+    PessoaRepositorio pessoaRep;
+
     public List<Pessoa> list() {
         Pessoa p = new Pessoa();
         p.setNome("Vinicius Audibert");
         p.setDataNascimento(new Date());
         return Stream.of(p).collect(Collectors.toList());
     }
-    
-    public Iterable<Pessoa> toList(){
-        return this.pessoaaRep.findAll();
+
+    public Iterable<Pessoa> toList() {
+        return this.pessoaRep.findAll();
+    }
+
+    public void save(Pessoa p) {
+        this.pessoaRep.save(p);
+    }
+
+    public Pessoa findById(Long id) {
+        return this.pessoaRep.findOne(id);
     }
 
 }
